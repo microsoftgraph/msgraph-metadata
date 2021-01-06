667b84adde2db69994c1ee0bdb89145eeb15febd
Mustafa Zengin <muzengin@microsoft.com>
2021-01-06 15:35:02 -0800

    metadata parser tool

---
+++ tools/MetadataParser/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.OData.Edm.Csdl;
+using System.Xml;
+using System;
+
+try
+{
+    CsdlReader.Parse(XmlReader.Create(args[0]));
+}
+catch(Exception e)
+{
+    Console.Error.WriteLine(e.Message);
+    Environment.Exit(-1);
+}
