cf7f8cb96259425d2798f59ca4af0df8251d85e3
Mustafa Zengin <muzengin@microsoft.com>
2021-01-27 15:08:22 -0800

    OpenAPI conversion step to metadata parser tool

--- tools/MetadataParser/Program.cs
+++ tools/MetadataParser/Program.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.OData.Edm.Csdl;
 using System.Xml;
 using System;
+using Microsoft.OData.Edm;
+using Microsoft.OpenApi.OData;
+using Microsoft.OpenApi.Models;
 
+IEdmModel edmModel;
 try
 {
-    CsdlReader.Parse(XmlReader.Create(args[0]));
+    edmModel = CsdlReader.Parse(XmlReader.Create(args[0]));
+    Console.WriteLine("Parsing the metadata as an edm model was successful!");
+
+    var settings = new OpenApiConvertSettings()
+    {
+        EnableKeyAsSegment = true,
+        EnableOperationId = true,
+        PrefixEntityTypeNameBeforeKey = true,
+        TagDepth = 2,
+        EnablePagination = true,
+        EnableDiscriminatorValue = false,
+        EnableDerivedTypesReferencesForRequestBody = false,
+        EnableDerivedTypesReferencesForResponses = false,
+        ShowRootPath = true,
+        ShowLinks = true
+    };
+
+    OpenApiDocument document = edmModel.ConvertToOpenApi(settings);
+
+    Console.WriteLine("Conversion to OpenAPI was successful!");
 }
-catch(Exception e)
+catch (Exception e)
 {
     Console.Error.WriteLine(e.Message);
     Environment.Exit(-1);
