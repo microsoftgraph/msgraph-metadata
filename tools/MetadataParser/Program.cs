569c0e5b82d7763912310603fc03e1bee6eb4d03
Mustafa Zengin <muzengin@microsoft.com>
2021-01-28 09:54:34 -0800

    add copyright header

--- tools/MetadataParser/Program.cs
+++ tools/MetadataParser/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.OData.Edm.Csdl;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.OData.Edm.Csdl;
 using System.Xml;
 using System;
 using Microsoft.OData.Edm;
