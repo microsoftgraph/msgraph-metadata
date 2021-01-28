e463e299de457e78ddb4bd2b2498c167e8c29c3c
Mustafa Zengin <muzengin@microsoft.com>
2021-01-28 11:21:04 -0800

    OpenAPI conversion validation as a PR gate (#63)
    * add OpenAPI OData Reader as a submodule
    
    * OpenAPI conversion step to metadata parser tool
    
    * add README
    
    * update validation steps
    
    * add broken XSLT to test the Github Action
    
    * fix copy-paste error
    
    * fix display name of a step
    
    * fix script path
    
    * github.workspace
    
    * Revert "add broken XSLT to test the Github Action"
    
    This reverts commit 890ffc59ca5c40253960165d7fa550f84cd88a2e.
    
    * - fixes open api update workflow
    
    * Auto-updates OpenAPI descriptions
    
    * add copyright header
    
    * allow empty in git commit
    
    * Auto-updates OpenAPI descriptions
    
    * don't allow empty commits, write a warning instead
    
    * add more coloring to output
    
    Co-authored-by: Vincent Biret <vibiret@microsoft.com>
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ scripts/run-metadata-validation.ps1
@@ -0,0 +1,48 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+<#
+.Synopsis
+    Runs MetadataParser tool to validate latest XSLT rules don't break downstream parsing of the metadata
+
+.Description
+    1. tranforms latests snapshots of beta and v1 metadata
+    2. validates that the transformed metadata is parsable as OData EDM model and conversion to OpenAPI document is possible
+
+.Example
+    ./scripts/run-metadata-validation.ps1 -repoDirectory C:/github/msgraph-metadata
+
+.Example
+    ./scripts/run-metadata-validation.ps1 -repoDirectory $GITHUB_WORKSPACE
+
+.Parameter repoDirectory
+    Full path the the root directory of msgraph-metadata checkout.
+#>
+
+param(
+    [Parameter(Mandatory=$true)][string]$repoDirectory
+)
+
+$transformCsdlDirectory = Join-Path $repoDirectory "transforms/csdl"
+$transformScript = Join-Path $transformCsdlDirectory "transform.ps1"
+$xsltPath = Join-Path $transformCsdlDirectory "preprocess_csdl.xsl"
+
+$betaSnapshot = Join-Path $repoDirectory "beta_metadata.xml"
+$v1Snapshot = Join-Path $repoDirectory "v1.0_metadata.xml"
+
+$metadataParserTool = Join-Path $repoDirectory "tools/MetadataParser/MetadataParser.csproj"
+
+$transformedBeta = Join-Path $repoDirectory "transformed_beta_metadata.xml"
+$transformedV1 = Join-Path $repoDirectory "transformed_v1.0_metadata.xml"
+
+Write-Host "Tranforming beta metadata using xslt..." -ForegroundColor Green
+& $transformScript -xslPath $xsltPath -inputPath $betaSnapshot -outputPath $transformedBeta
+
+Write-Host "Validating beta metadata after the transform..." -ForegroundColor Green
+& dotnet run -p $metadataParserTool $transformedBeta
+
+Write-Host "Tranforming v1.0 metadata using xslt..." -ForegroundColor Green
+& $transformScript -xslPath $xsltPath -inputPath $v1Snapshot -outputPath $transformedV1
+
+Write-Host "Validating v1.0 metadata after the transform..." -ForegroundColor Green
+& dotnet run -p $metadataParserTool $transformedV1
